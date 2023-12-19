43cb69dafa074652b1536b1de6a2606a81c3c568
coconutpalmtreeisland <adorableness1215@naver.com>
2023-12-19 19:24:10 +0900

    23.12.19

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,22 +1,7 @@
-$(function () {
-    $(".header__right").mouseover(function () {
-        $(this).find(".img1").stop().mouseover(function () {
-            $(".img1").fadeIn(200).addClass("on");
-        });
-        $(this).find(".img1").stop().mouseout(function () {
-            $(".img1").removeClass("on");
-        });
-        $(this).find(".img2").stop().mouseover(function () {
-            $(".img2").fadeIn(200).addClass("on");
-        });
-        $(this).find(".img2").stop().mouseout(function () {
-            $(".img2").removeClass("on");
-        });
-        $(this).find(".img3").stop().mouseover(function () {
-            $(".img3").fadeIn(200).addClass("on");
-        });
-        $(this).find(".img3").stop().mouseout(function () {
-            $(".img3").removeClass("on");
-        });
-    });
-});
+import { headerImg } from "./headerImg.js";
+import { smooth } from "./smooth.js";
+
+window.addEventListener("load", function () {
+    headerImg();
+    smooth();
+})
