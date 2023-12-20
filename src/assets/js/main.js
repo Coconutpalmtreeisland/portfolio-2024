9fa407ea83a4b4858d7de1e0e27a1e3608e83ebe
coconutpalmtreeisland <adorableness1215@naver.com>
2023-12-20 20:28:05 +0900

    23.12.20

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,11 @@
 import { headerImg } from "./headerImg.js";
 import { smooth } from "./smooth.js";
+import { line } from "./line.js";
 import { site } from "./site.js";
 
 window.addEventListener("load", function () {
     headerImg();
     smooth();
+    line();
     site();
 })
