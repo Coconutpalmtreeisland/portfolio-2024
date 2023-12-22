c25a9165a700bc7be60f60fcecb49b0f3916d495
coconutpalmtreeisland <adorableness1215@naver.com>
2023-12-22 19:02:02 +0900

    23.12.22

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,15 @@
 import { headerImg } from "./headerImg.js";
 import { smooth } from "./smooth.js";
 import { line } from "./line.js";
-import { site } from "./site.js";
+// import { site } from "./site.js";
+import { polaroid } from "./polaroid.js";
+import { hideHeaderImg } from "./hideHeaderImg.js";
 
 window.addEventListener("load", function () {
     headerImg();
     smooth();
     line();
-    site();
+    // site();
+    polaroid();
+    hideHeaderImg();
 })
